086c83bf4a41274b1ff5e6760b325e73ed9d85e7
maitreya526 <57538330+maitreya526@users.noreply.github.com>
2023-02-15 19:47:49 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,5 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout << "This is Maitreya\n";
-  cout << "My SRN is PES1UG20CS244\n";
   return 0;
 }
